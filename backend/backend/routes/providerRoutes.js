9c2b6d65464583ccfc1cba1dd4e93762aa66a6fc
Divyansh Jain <divyansh05j@gmail.com>
2025-11-22 15:32:49 +0530

    authentication routres fixed

--- backend/backend/routes/providerRoutes.js
+++ backend/backend/routes/providerRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { protect, providerOnly } = require('../middleware/authMiddleware');
+const { protect, providerOnly } = require('./Authmiddleware');
 const User = require('../models/User');
 const router = express.Router();
 
