f994aa0cdee6ead40d9cd381870e60455511f6a2
divyansh_jain <138308305+divyanshj0@users.noreply.github.com>
2025-11-22 16:56:14 +0530

    Modify server.js for Vercel compatibility
    Comment out server port configuration and add export for Vercel compatibility.

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -39,5 +39,7 @@ app.get('/api/public-health', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Add this line for Vercel compatibility:
+module.exports = app;
